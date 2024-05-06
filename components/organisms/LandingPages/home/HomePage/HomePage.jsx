b58ea834c3aaa84376e57fbc6fbfb149392ceddc
giffy onyinye <giffyonyinye@gmail.com>
2024-05-06 11:41:05 +0100

    new dev

--- components/organisms/LandingPages/home/HomePage/HomePage.jsx
+++ components/organisms/LandingPages/home/HomePage/HomePage.jsx
@@ -5,17 +5,17 @@ import MailListingSection from "@components/organisms/LandingPages/home/MailList
 // import OpportunitySection from "@components/organisms/LandingPages/home/OpportunitySection/OpportunitySection";
 import StartSection from "@components/organisms/LandingPages/home/StartSection/StartSection";
 import TopSection from "@components/organisms/LandingPages/home/TopSection/TopSection";
-// import WhySection from "@components/organisms/LandingPages/home/WhySection/WhySection";
+import WhySection from "@components/organisms/LandingPages/home/whySection/WhySection";
 
 import HavannaProvision from "../HavannaProvision/HavannaProvision";
-// import WhySection from "../WhySection/WhySection";
+// import WhySection from "../whySection/WhySection";
 
 const HomePage = () => {
   return (
     <div>
       <TopSection />
       <HavannaProvision />
-      {/* <WhySection /> */}
+      <WhySection />
       <StartSection />
       {/* <OpportunitySection /> */}
       <FutureSection />
